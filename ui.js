b7a091124772aa4969949b1409ff8a4cbb7915e4
Henrique Heron <henriqueheron1@gmail.com>
2024-01-21 13:50:35 -0300

    refatorar: opção Selecionar no começo

--- ui.js
+++ ui.js
@@ -1,14 +1,20 @@
 export function mostrarDescricao(dados) {
-    const cursoSelecionado = document.querySelector("#curso")
-    const descricao = document.querySelector("#descricao")
-    const gradeCurricular = document.querySelector('#grade-curricular')
-    const linkGradeCurricular = document.querySelector('#link-grade-curricular')
-    
-    for(const obj in dados) {
-        if (dados[obj].sigla === cursoSelecionado.value.toUpperCase()) {
-            descricao.textContent = dados[obj].descricao;
-            gradeCurricular.textContent = dados[obj].linkGrade;
-            linkGradeCurricular.href = dados[obj].linkGrade;
-        }
+  const cursoSelecionado = document.querySelector("#curso");
+  const descricao = document.querySelector("#descricao");
+  const gradeCurricular = document.querySelector("#grade-curricular");
+  const linkGradeCurricular = document.querySelector("#link-grade-curricular");
+
+  if (cursoSelecionado.value === "Selecione") {
+    descricao.textContent = "";
+    gradeCurricular.textContent = "";
+    linkGradeCurricular.href = "#";
+  }
+
+  for (const obj in dados) {
+    if (dados[obj].sigla === cursoSelecionado.value.toUpperCase()) {
+      descricao.textContent = dados[obj].descricao;
+      gradeCurricular.textContent = dados[obj].linkGrade;
+      linkGradeCurricular.href = dados[obj].linkGrade;
     }
+  }
 }
