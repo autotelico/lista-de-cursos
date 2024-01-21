fc854544577460a77dd3d63a71d4518cac8b244d
Henrique Heron <henriqueheron1@gmail.com>
2024-01-21 11:29:15 -0300

    feat: adicionar link à grade curricular

--- index.js
+++ index.js
@@ -2,21 +2,12 @@ import { buscarDados } from './api.js';
 import { mostrarDescricao } from './ui.js';
 
 document.addEventListener("DOMContentLoaded", () => {
-    const cursoSelecionado = document.querySelector("#curso")
-    const descricao = document.querySelector("#descricao")
-    const gradeCurricular = document.querySelector('#grade-curricular')
     
     
     cursoSelecionado.addEventListener('change', async () => {
         const dados = await buscarDados()
         console.log(dados);
-        console.log(cursoSelecionado.value);
-        for(const obj in dados) {
-            if (dados[obj].sigla === cursoSelecionado.value.toUpperCase()) {
-                descricao.textContent = dados[obj].descricao;
-                gradeCurricular.textContent = dados[obj].linkGrade;
-            }
-        }
+        mostrarDescricao(dados);
 
 
     })
--- ui.js
+++ ui.js
@@ -1,3 +1,14 @@
-export function mostrarDescricao() {
-  
+export function mostrarDescricao(dados) {
+    const cursoSelecionado = document.querySelector("#curso")
+    const descricao = document.querySelector("#descricao")
+    const gradeCurricular = document.querySelector('#grade-curricular')
+    const linkGradeCurricular = document.querySelector('#link-grade-curricular')
+    
+    for(const obj in dados) {
+        if (dados[obj].sigla === cursoSelecionado.value.toUpperCase()) {
+            descricao.textContent = dados[obj].descricao;
+            gradeCurricular.textContent = dados[obj].linkGrade;
+            linkGradeCurricular.href = dados[obj].linkGrade;
+        }
+    }
 }
