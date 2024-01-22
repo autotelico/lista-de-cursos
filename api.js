9d1be1c3b9c26c23b398c03838e8e809f6b2d23e
Henrique Heron <henriqueheron1@gmail.com>
2024-01-21 23:42:18 -0300

    feat: adicionar aviso de busca em banco de dados

--- api.js
+++ api.js
@@ -1,8 +1,13 @@
+const avisoAPI = document.querySelector("#aviso-api");
+
 export async function buscarDados() {
+  avisoAPI.textContent =
+  "Buscando informações no banco de dados. Por favor, aguarde...";
   let response = await fetch(
     "https://api-gerenciador-dce.herokuapp.com/curso",
     { mode: "cors" }
   );
   let data = await response.json();
+  avisoAPI.textContent = '';
   return data;
 }
